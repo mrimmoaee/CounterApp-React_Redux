ebe2ceb80c690bc6fe31635b5d6fbf8875a0c00a
Mrimmoaee Ahmed Shame <mrimmoaee.shame@gogames.co>
2020-02-09 16:15:58 +0600

    Updates

---
+++ precache-manifest.c4bd740061c30a8af9777918e39412b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f133d7119b57d5cd1d274bd4cd227e",
+    "url": "/index.html/index.html"
+  },
+  {
+    "revision": "3e4314889b16b9bb1330",
+    "url": "/index.html/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "120933a3a5414b97b83c",
+    "url": "/index.html/static/js/2.e381bac7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/index.html/static/js/2.e381bac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e4314889b16b9bb1330",
+    "url": "/index.html/static/js/main.02ed6216.chunk.js"
+  },
+  {
+    "revision": "0e3256ba325ff96c56a2",
+    "url": "/index.html/static/js/runtime-main.a5967d1e.js"
+  }
+]);
